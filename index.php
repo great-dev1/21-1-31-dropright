bcb167e47beb37e25077309bbf51045cd638a47c
Twenty-August <twentytwentyaugust@outlook.com>
2021-01-31 07:35:33 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Collapsed Sidebar</title>
+
+  <!-- Google Fonts -->
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap">
+
+  <!-- My CSS -->
+  <link rel="stylesheet" href="assets/css/purpose-blue-light.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+  <!-- <img src="../Capture.PNG" style="position: absolute; z-index: 99; top: 0; left: 0; opacity: 0.7"> -->
+
+  <span class="drop-right">
+    <button class="hover-btn">Hover me</button>
+
+    <ul class="drop-right-menu">
+      <li>
+        <a class="drop-right-item" href="#">Agency</a>
+      </li>
+
+      <li>
+        <a class="drop-right-item" href="#">App</a>
+      </li>
+
+      <li>
+        <a class="drop-right-item" href="#">Blog</a>
+      </li>
+
+      <li>
+        <a class="drop-right-item" href="#">Classic</a>
+      </li>
+
+      <li>
+        <a class="drop-right-item" href="#">Cloud hosting</a>
+      </li>
+    </ul>
+  </span>
+
+  <!-- jQuery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+  <!-- Popper -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+  <!-- Bootstrap -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+  <!-- My JavaScript -->
+  <script src="assets/js/script.js"></script>
+</body>
+
+</html>
